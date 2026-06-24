a63664dc7823ac6fb1d42d5d8ac7521e9cf544e0
abhishekshinde400705-git <abhishekshinde400705@gmail.com>
2026-06-24 23:24:07 +0530

    Add files via upload

---
+++ JAVA/BASIC/mul.java
@@ -0,0 +1,21 @@
+package basic;
+import java.util.Scanner;
+public class mul {
+	
+	public static void main(String[]agre)
+	{
+		
+		Scanner abhi = new Scanner(System.in);
+		 
+		int num1,num2,mul;
+		
+		System.out.println("Enter your first no:");
+		num1=abhi.nextInt();
+		
+		System.out.println("Enter your second no:");
+		num2=abhi.nextInt();
+		
+		System.out.println("multiplication of two no is:"+ (num1*num2));		
+		
+	}
+}
