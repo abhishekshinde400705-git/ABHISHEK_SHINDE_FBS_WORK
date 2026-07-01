310e074424aa5559a013686f5c930e82db8cdb23
abhishekshinde400705-git <abhishekshinde400705@gmail.com>
2026-07-01 22:52:32 +0530

    Add files via upload

---
+++ JAVA SCRIPT/basic/mul.js
@@ -0,0 +1,5 @@
+let num1,num2,result;
+num1 = parseFloat(prompt("Enter the first number:"));
+num2 = parseFloat(prompt("Enter the second number:"));
+result = num1 * num2;
+console.log("The result of multiplication is: " ,result);
