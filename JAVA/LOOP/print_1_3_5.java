1707f8f6cb579fd8a98308fbcb317ba528dc838e
abhishekshinde400705-git <abhishekshinde400705@gmail.com>
2026-07-04 23:07:12 +0530

    Add files via upload

---
+++ JAVA/LOOP/print_1_3_5.java
@@ -0,0 +1,13 @@
+package loop;
+import java.util.Scanner;
+public class print_1_3_5 {
+	
+	public static void main(String[]agre)
+	{
+		for(int i=1;i<=50;i+=3)
+		{
+			System.out.println(i);
+		}
+	}
+
+}
