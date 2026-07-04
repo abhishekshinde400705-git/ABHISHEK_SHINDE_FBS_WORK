246242d7ec06cb4bea8560ffca3fdad246dc230d
abhishekshinde400705-git <abhishekshinde400705@gmail.com>
2026-07-04 23:05:49 +0530

    Add files via upload

---
+++ JAVA/LOOP/even_no.java
@@ -0,0 +1,17 @@
+package loop;
+import java.util.Scanner;
+public class even_no {
+
+	public static void main(String[]agre)
+	{
+		for(int i=1;i<=100;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println(i);	
+			}
+			
+		}
+	}
+
+}
