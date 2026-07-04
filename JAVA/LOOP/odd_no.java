9746b723af41173b46178bddb3358ebaf31e6a59
abhishekshinde400705-git <abhishekshinde400705@gmail.com>
2026-07-04 23:06:15 +0530

    Add files via upload

---
+++ JAVA/LOOP/odd_no.java
@@ -0,0 +1,17 @@
+package loop;
+import java.util.Scanner;
+public class odd_no {
+
+	public static void main(String[]agre)
+	{
+		for(int i=1;i<=100;i++)
+		{
+			if(i%2!=0)
+			{
+				System.out.println(i);	
+			}
+			
+		}
+	}
+
+}
