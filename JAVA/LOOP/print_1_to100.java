8e1e67e7b81b6e734d850d67c339dd0ccad9bf7a
abhishekshinde400705-git <abhishekshinde400705@gmail.com>
2026-07-05 10:07:05 +0530

    Add files via upload

---
+++ JAVA/LOOP/print_1_to100.java
@@ -0,0 +1,13 @@
+package loop;
+import java.util.Scanner;
+public class print_1_to100 {
+	
+	public static void main(String[]agre)
+	{
+		for(int i=0;i<=100;i++)
+		{
+			System.out.println(i);
+		}
+	}
+
+}
