83a96b65cf2c32f28a8dc5d169e1a376bc229980
abhishekshinde400705-git <abhishekshinde400705@gmail.com>
2026-07-10 23:05:15 +0530

    Add files via upload

---
+++ dsa/array/cout_pos.java
@@ -0,0 +1,21 @@
+package DSA;
+
+public class cout_pos {
+	public static void main(String[]agre)
+	{
+		int abhi[]= {1,2,-9,2,-6,-11,5,1,2,-66,-55};
+		
+		int count=0;
+		
+		for(int i=0;i<abhi.length;i++)
+		{
+			if(abhi[i]>=0)
+			{
+				count++;
+			}
+		}
+		
+		System.out.print(count);
+	}
+
+}
