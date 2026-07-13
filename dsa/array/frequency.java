df3c3094ab536437470653fcf50a486f3239c698
abhishekshinde400705-git <abhishekshinde400705@gmail.com>
2026-07-13 22:00:18 +0530

    Add files via upload

---
+++ dsa/array/frequency.java
@@ -0,0 +1,24 @@
+package abhidada;
+
+public class frequency {
+	
+	public static void main(String[]agre)
+	{
+		int abhi[]= {1,2,3,4,4,6,7,8,9,0};
+		int no=6;
+		
+		for(int i=0;i<abhi.length;i++)
+		{
+			
+			if(abhi[i]==5)
+			{
+				System.out.print("element is found");
+				return;
+			}
+			
+		}
+		System.out.print("element is not found");
+		}
+		
+	}
+
